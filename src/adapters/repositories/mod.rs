cb1c4a95fb071fbf7ea1ad27a1c570ab9a28f4e7
Ang2Tea <angtwotea@gmail.com>
2025-06-10 10:57:14 +0300

    refactor: Delete sqlite provider

--- src/adapters/repositories/mod.rs
+++ src/adapters/repositories/mod.rs
@@ -3,8 +3,5 @@ pub mod in_memory;
 #[cfg(feature = "postgres")]
 pub mod postgres;
 
-#[cfg(feature = "sqlite")]
-pub mod sqlite;
-
 #[cfg(feature = "sqlx")]
 pub mod sqlx_helper;
--- src/adapters/repositories/sqlite.rs
+++
@@ -1,3 +0,0 @@
-pub mod chat_repository;
-
-pub use chat_repository::*;
